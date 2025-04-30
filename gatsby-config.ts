bc7aa385683c42f9b4894518fd8b4d6408f87fb2
Yaw Agyepong <yaw.agyepong@gmail.com>
2025-04-30 11:52:46 -0400

    add path prefix to gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "gatsby-pwa-poc",
   siteMetadata: {
     title: `gatsby-pwa-poc`,
     siteUrl: `https://www.yourdomain.tld`
